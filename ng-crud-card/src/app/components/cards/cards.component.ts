e26a0d24dad1e85e83c5480de3221a8a3c36e3df
ROBERT-Gimenez <robertgim37@gmail.com>
2023-03-28 23:42:28 -0300

    added directive for mapping list data

--- ng-crud-card/src/app/components/cards/cards.component.ts
+++ ng-crud-card/src/app/components/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { CardModel } from 'src/app/model/card-model';
 import { CardService } from 'src/app/service/card.service';
 
@@ -11,10 +12,20 @@ export class CardsComponent implements OnInit {
 
 
 listCard:CardModel [] = [];
+formCard:FormGroup = new FormGroup({});
+
 constructor(private cardService:CardService){}
 
 ngOnInit(): void {
   this.list();
+  this.formCard = new FormGroup({
+    id_card: new FormControl(''),
+    name: new FormControl(''),
+    number: new FormControl(''),
+    type: new FormControl(''),
+    cvv: new FormControl(''),
+    status: new FormControl('1'),
+  });
 }
 list(){
   this.cardService.getCards().subscribe(res=> {
