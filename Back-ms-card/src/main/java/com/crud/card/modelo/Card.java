5916f341bc3066ec8d43b810c151354a0b97976b
robert giemenz <you@example.com>
2023-04-06 19:27:21 -0300

    edited the model to create table columns

--- Back-ms-card/src/main/java/com/crud/card/modelo/Card.java
+++ Back-ms-card/src/main/java/com/crud/card/modelo/Card.java
@@ -2,13 +2,29 @@ package com.crud.card.modelo;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cards")
 @Data
 public class Card {
-    int id_card;
-    String name;
-    String number;
-    String type;
-    int cvv;
-    int status;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_card;
+
+    @Column
+    private String name;
+
+    @Column
+    private String number;
+
+    @Column
+    private String type;
+
+    @Column
+    private int cvv;
+
+    @Column
+    private int status;
 
 }
