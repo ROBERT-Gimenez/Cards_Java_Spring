975c427903a77a552879111e1efa2493d862e20d
ROBERT-Gimenez <robertgim37@gmail.com>
2023-04-07 21:19:14 -0300

    added PendingController

--- Back-ms-card/src/main/java/com/crud/card/controller/PendingController.java
+++ Back-ms-card/src/main/java/com/crud/card/controller/PendingController.java
@@ -1,12 +1,15 @@
 package com.crud.card.controller;
 
+import com.crud.card.modelo.ModelService;
+import com.crud.card.modelo.Pendientes;
+import com.crud.card.modelo.UserModel;
 import com.crud.card.repositorio.PendingRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +19,37 @@ public class PendingController {
     private PendingRepo pendingService;
 
     @GetMapping("allPending")
-    public ResponseEntity<>
+    public ResponseEntity<List<Pendientes>> List(){
+        var result = pendingService.allPending();
+        return new ResponseEntity<>(result , HttpStatus.OK);
+    }
+
+    @PostMapping("/addedPending")
+    public ResponseEntity<ModelService> saveUser(@RequestBody Pendientes file) {
+        ModelService messageResp = new ModelService();
+        pendingService.save(file);
+        messageResp.setSuccess(true);
+        messageResp.setMessage("File saved with success");
+        return ResponseEntity.ok(messageResp);
+    }
+    @PutMapping("/UpdatePending")
+    public ResponseEntity<ModelService> update(@RequestBody Pendientes file){
+        ModelService messageResp = new ModelService();
+        int result = pendingService.update(file);
+        if(result == 1){
+            messageResp.setSuccess(true);
+            messageResp.setMessage("File Update with success");
+        }
+        return new ResponseEntity<>(messageResp,HttpStatus.OK);
+    }
+    @PutMapping("/AcceptPending/{id}")
+    public ResponseEntity<ModelService> approveFile(@PathVariable int id){
+        ModelService messageResp = new ModelService();
+        int result = pendingService.approveFile(id);
+        if(result==1){
+            messageResp.setSuccess(true);
+            messageResp.setMessage(" File Approve with success");
+        }
+        return new ResponseEntity<>(messageResp, HttpStatus.OK);
+    }
 }
